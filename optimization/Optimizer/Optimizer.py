d0c5e4fe5d29e61a9214d7056befe8c802ffcb94
SebastiaanV <sebastiaan.vermeulen@ortec-finance.com>
2017-07-25 19:24:50 +0200

    towards a working simulator

--- optimization/Optimizer/Optimizer.py
+++ optimization/Optimizer/Optimizer.py
@@ -4,6 +4,9 @@ import numpy as np
 import json
 from scipy import stats, optimize
 from matplotlib import pyplot as plt
+import multiprocessing as mp
+from multiprocessing import Process, Queue
+import time
 
 class Optimizer(object):
     con = sqlite3.connect(r'D:\data\scriptie\optimization.sqlite')
@@ -47,9 +50,9 @@ def iv_to_call(df):
     d2 = lambda s, k, ss, t: d1(s,k, ss, t) - ss*np.sqrt(t)
     bs = lambda s, k, ss, t: stats.norm.cdf(d1(s, k, ss, t))*s - stats.norm.cdf(d2(s, k, ss, t))*k
     data = df.copy()
-    for t in data.columns:
-        for k in data.index:
-            data.loc[k, t] = bs(1, k, data.loc[k, t]/100, t) #impvol to BS price
+    for k in data.columns:
+        for t in data.index:
+            data.loc[t, k] = bs(1, k, data.loc[t, k], t) #impvol to BS price
     return data
 
 
@@ -86,6 +89,25 @@ class CheyetteCalibration(object):
         return cheyette(*self.x.x, *self.W.shape, self.W, self.dt, self.f)
 
 
+def _error(Ml, Mu, S, k, C):
+    """
+    Params
+    -----
+    C : call option price at maturity T
+    k : option strike
+    Ml: lower-bound discount factor at maturity T
+    Mu: upper-bound discount factor at maturity T
+    S : asset price at maturity T
+    
+    All except k, C are expected to be arrays of simulated points.
+
+    Returns
+    -----
+    absolute error of crossing the bound
+    """
+    return max(0, C - np.mean(Mu*np.maximum(0, S-k))) + max(0, np.mean(Ml*np.maximum(0, S-k)) - C)
+
+
 class Calibration(object):
     snu = np.array([1,0,0])
     sV  = np.array([0,1,0])
@@ -94,17 +116,17 @@ class Calibration(object):
         self.r_params = [r_xi, r_kappa, r_f]
         self.data = iv_to_call(data)
         
-    def calculate(self, qS, kappa_v, xi_v, sigma_v, delta, sS):
+    def calculate(self, qS, kappa_v, xi_v, sigma_v, delta, sS, gamma=0):
         V = 1 + np.sqrt( CIR(xi_v, self.W[:,:,0], self.discretization_steps, self.n_simulations, self.dt, kappa_v, xi_v, sigma_v)[:-1] )
-        beta = -qS / np.inner(sS, sS)
+        beta = -qS / np.inner(sS, sS) / V
         S = np.cumprod(1 + (self.r+qS)*self.dt + (self.W @ self.sV) * V , 0)
-        p1 = beta * (self.W @ sS) / V
-        p2 = self.W @ (delta*self.snu)
-        p3 = p1 + p2
-        M = np.cumprod(1 -  self.r    *self.dt + p3, 0)
-        return S, M
+        p3 = beta * (self.W @ sS) + self.W @ (delta*self.snu)
+        lu = self.W @ (gamma * self.sV)
+        Mu = np.cumprod(1 -  self.r    *self.dt + p3 + lu, 0)
+        Ml = np.cumprod(1 -  self.r    *self.dt + p3 - lu, 0)
+        return S, Mu, Ml
 
-    def calibrate(self, dt=.01, n_simulations=100, T=10, con=None):
+    def calibrate(self, dt=.01, n_simulations=100, T=10, con=None, q=None):
         if T < self.data.columns.max():
             raise ValueError("max maturity too small for data")
         self.discretization_steps = int(T/dt)+1
@@ -115,45 +137,32 @@ class Calibration(object):
         self.r = cheyette(*self.r_params[:2], *self.Wr.shape, self.Wr, self.dt, self.r_params[2])
 
         def fun(args):
-            S, M = self.calculate(*args[:5], np.r_[args[5:8]])
-            fit = np.sum(np.square([
-                self.data.loc[k, t] - np.mean(M[int(t/dt)]*np.maximum(0, S[int(t/dt)]-k))
+            S, Mu, Ml = self.calculate(*args[:5], np.r_[args[5:8]], args[8])
+            fit = max(
+                _error(Ml[int(t/dt)], Mu[int(t/dt)], S[int(t/dt)], k, self.data.loc[k, t])
                 if not np.isnan(self.data.loc[k, t])
                 else 0
                 for k in self.data.index for t in self.data.columns
-            ]))
-            print('got fit ', fit)
-            return fit
+            )
+            if q:
+                q.put((fit,args[8]))
+            return fit*1e5 + args[8]**2
 
-        self.x = optimize.minimize(fun, np.random.exponential(1, 8), method='nelder-mead') 
+
+        self.x = optimize.minimize(fun, np.random.exponential(1, 9), method='L-BFGS-B', bounds=[(None, None), (0, None), (0, None), (None, None), (None, None), (None, None), (None, None), (0, None)]) 
         if con is not None:
             con.execute(
-                '''insert into heston(qS, kappa_v, xi_v, sigma_v, delta, sS1, sS2, sS3, p, dt, kappa_r, xi_r, f, fun)
-                values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', [*self.x.x, self.n_simulations, self.dt, *self.r_params, self.x.fun])
+                '''insert into heston(qS, kappa_v, xi_v, sigma_v, delta, sS1, sS2, sS3, gamma, p, dt, kappa_r, xi_r, f, fun)
+                values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', [*self.x.x, self.n_simulations, self.dt, *self.r_params, self.x.fun])
             con.commit()
         return self.x
 
 
-if __name__ == '__main__':
+def do_calibration(q, t):
     con = sqlite3.connect('results.sqlite')
     con.execute('create table if not exists cheyette(kappa real, xi real, created datetime default current_timestamp, p real, dt real, f real, fun real)')
-    con.execute('create table if not exists heston(qS real, kappa_v real, xi_v real, sigma_v real, delta real, sS1 real, sS2 real, sS3 real, created datetime default current_timestamp, p real, dt real, kappa_r real, xi_r real, f real, fun real)')
+    con.execute('create table if not exists heston(qS real, kappa_v real, xi_v real, sigma_v real, delta real, sS1 real, sS2 real, sS3 real, gamma real, created datetime default current_timestamp, p real, dt real, kappa_r real, xi_r real, f real, fun real)')
     forward_curve = .002
-
-    print('started')
-    with open('usd_swaption_data.json') as f:
-        js = json.load(f)
-    df = pd.Panel(js).loc[:,'log-normal vol',:]
-    df.index = df.index.str.replace('ATM','0.00%').str.replace('%','').astype('float')/100 + 1
-    df.columns = df.columns.astype(int)
-    print('initializing')
-    cc = CheyetteCalibration(df)
-    print('started optimization')
-    print(cc.calibrate(p=10000, f=forward_curve, con=con)) #  0.9912468 ,  1.29428977
-
-    #plt.plot(cheyette(*cc.x.x, 100, 10, np.random.normal(0, .1, (100,10)), .01, forward_curve))
-    #plt.plot(np.mean(cheyette(*cc.x.x, 100, 10, np.random.normal(0, .1, (100,10)), .01, forward_curve),1))
-    
     with open('spx_option_data.json') as f:
         js = json.load(f)
     df = pd.Panel(js).loc[:,:,'2017/06/30']
@@ -162,7 +171,42 @@ if __name__ == '__main__':
     df.index = df.index.days/365
     df = df[df.index>0]
     df /= 100
-    ca = Calibration(*cc.x.x, forward_curve, df.T)
-    print(ca.calibrate(n_simulations=10000, con=con))
+    ca = Calibration(0.0212, 0.00445, forward_curve, df)
+    while time.time() < t:
+        ca.calibrate(n_simulations=10000, con=con, q=q)
+
+
+
+if __name__ == '__main__':
+    con = sqlite3.connect('results.sqlite')
+    con.execute('create table if not exists cheyette(kappa real, xi real, created datetime default current_timestamp, p real, dt real, f real, fun real)')
+    con.execute('create table if not exists heston(qS real, kappa_v real, xi_v real, sigma_v real, delta real, sS1 real, sS2 real, sS3 real, gamma real, created datetime default current_timestamp, p real, dt real, kappa_r real, xi_r real, f real, fun real)')
+    forward_curve = .002
+
+    if False:
+        print('started')
+        with open('usd_swaption_data.json') as f:
+            js = json.load(f)
+        df = pd.Panel(js).loc[:,'log-normal vol',:]
+        df.index = df.index.str.replace('ATM','0.00%').str.replace('%','').astype('float')/100 + 1
+        df.columns = df.columns.astype(int)
+        print('initializing')
+        cc = CheyetteCalibration(df.T)
+        print('started optimization')
+        print(cc.calibrate(p=10000, f=forward_curve, con=con)) #  0.9912468 ,  1.29428977
+
+    #plt.plot(cheyette(*cc.x.x, 100, 10, np.random.normal(0, .1, (100,10)), .01, forward_curve))
+    #plt.plot(np.mean(cheyette(*cc.x.x, 100, 10, np.random.normal(0, .1, (100,10)), .01, forward_curve),1))
+    t = time.time()+60
+    queues = [Queue() for _ in range(2)]
+    procs = [Process(target=do_calibration, args=(q, t)) for q in queues]
+    for p in procs:
+        p.start()
+    while any(p.is_alive() for p in procs):
+        out = [q.get() for q, p in zip(queues, procs) if p.is_alive() or not q.empty()]
+        print('---')
+        print('violation', ' '.join(f'{c[0]:12.8f}' for c in out))
+        print('gamma    ', ' '.join(f'{c[1]:12.8f}' for c in out))
+
 
 
