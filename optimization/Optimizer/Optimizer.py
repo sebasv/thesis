38f785f504dcb10714ffcaa0050abdc9006d4c35
Sebastiaan Vermeulen <mail@sebastiaanvermeulen.nl>
2017-07-25 23:01:30 +0200

    fixed min vola error

--- optimization/Optimizer/Optimizer.py
+++ optimization/Optimizer/Optimizer.py
@@ -8,9 +8,9 @@ import multiprocessing as mp
 from multiprocessing import Process, Queue
 import time
 
-class Optimizer(object):
-    con = sqlite3.connect(r'D:\data\scriptie\optimization.sqlite')
-    df = pd.read_csv(r'd:\data\scriptie\spx_option_quotes.csv')
+# class Optimizer(object):
+#     con = sqlite3.connect(r'D:\data\scriptie\optimization.sqlite')
+#     df = pd.read_csv(r'd:\data\scriptie\spx_option_quotes.csv')
 
 
 def estimate_short_rate():
@@ -112,18 +112,28 @@ class Calibration(object):
     snu = np.array([1,0,0])
     sV  = np.array([0,1,0])
 
-    def __init__(self, r_xi, r_kappa, r_f, data):
+    def __init__(self, r_xi, r_kappa, r_f, con):
         self.r_params = [r_xi, r_kappa, r_f]
-        self.data = iv_to_call(data)
+        df = pd.read_sql('select * from option_data', con, index_col='index')
+        df.columns = df.columns.astype(float)
+        self.data = df
+
+    def efficient(self, qS, kappa_v, xi_v, sigma_v, delta, sS):
+        V = 1 + np.sqrt( CIR(xi_v, self.W[:, :, 0], self.discretization_steps, self.n_simulations, self.dt, kappa_v, xi_v, sigma_v)[:-1] )
+        beta = -qS / np.inner(sS, sS) / V
+        S = np.cumprod(1 + (self.r+qS)*self.dt + (self.W @ sS) * V, 0)
+        p3 = beta * (self.W @ sS) + self.W[:, :, 0] * delta
+        M = np.cumprod(1 - self.r * self.dt + p3, 0)
+        return S, M
         
-    def calculate(self, qS, kappa_v, xi_v, sigma_v, delta, sS, gamma=0):
+    def inefficient(self, qS, qV, kappa_v, xi_v, sigma_v, delta, sS, gamma, sV):
         V = 1 + np.sqrt( CIR(xi_v, self.W[:,:,0], self.discretization_steps, self.n_simulations, self.dt, kappa_v, xi_v, sigma_v)[:-1] )
         beta = -qS / np.inner(sS, sS) / V
-        S = np.cumprod(1 + (self.r+qS)*self.dt + (self.W @ self.sV) * V , 0)
+        S = np.cumprod(1 + (self.r+qV)*self.dt + self.W[:, :, 1] * V * sV , 0)
         p3 = beta * (self.W @ sS) + self.W @ (delta*self.snu)
         lu = self.W @ (gamma * self.sV)
-        Mu = np.cumprod(1 -  self.r    *self.dt + p3 + lu, 0)
-        Ml = np.cumprod(1 -  self.r    *self.dt + p3 - lu, 0)
+        Mu = np.cumprod(1 - self.r * self.dt + p3 + lu, 0)
+        Ml = np.cumprod(1 - self.r * self.dt + p3 - lu, 0)
         return S, Mu, Ml
 
     def calibrate(self, dt=.01, n_simulations=100, T=10, con=None, q=None):
@@ -136,33 +146,44 @@ class Calibration(object):
         self.Wr = np.random.normal(scale=np.sqrt(self.dt), size=(self.discretization_steps-1, n_simulations))
         self.r = cheyette(*self.r_params[:2], *self.Wr.shape, self.Wr, self.dt, self.r_params[2])
 
-        def fun(args):
-            S, Mu, Ml = self.calculate(*args[:5], np.r_[args[5:8]], args[8])
-            fit = max(
-                _error(Ml[int(t/dt)], Mu[int(t/dt)], S[int(t/dt)], k, self.data.loc[k, t])
-                if not np.isnan(self.data.loc[k, t])
-                else 0
-                for k in self.data.index for t in self.data.columns
-            )
-            if q:
-                q.put((fit,args[8]))
-            return fit*1e5 + args[8]**2
+        def eff(args):
+            S, M = self.efficient(*args[:5], np.r_[args[5:7], 0])
+            fit = np.sum(np.square([
+                np.mean(M[int(t/dt)]*np.maximum(S[int(t/dt)]-k, 0)) - self.data.loc[t, k]
+                for t in self.data.index for k in self.data.columns
+            ]))
+            print(f'got fit {fit:12.8f}')
+            return fit
+
+        # def fun(args):
+        #     S, Mu, Ml = self.calculate(*args[:5], np.r_[args[5:8]], args[8], args[9])
+        #     fit = max(
+        #         _error(Ml[int(t/dt)], Mu[int(t/dt)], S[int(t/dt)], k, self.data.loc[k, t])
+        #         if not np.isnan(self.data.loc[k, t])
+        #         else 0
+        #         for k in self.data.index for t in self.data.columns
+        #     )
+        #     if q:
+        #         q.put((fit,args[8]))
+        #     return fit*1e5 + args[8]**2
 
 
-        self.x = optimize.minimize(fun, np.random.exponential(1, 9), method='L-BFGS-B', bounds=[(None, None), (0, None), (0, None), (None, None), (None, None), (None, None), (None, None), (0, None)]) 
+        self.x = optimize.minimize(
+            eff, 
+            np.random.exponential(.1, 7), 
+            method='nelder-mead', 
+            #bounds=[(None, None), (0, None), (0, None), (0, None), (None, None), (None, None), (None, None), (None, None), (0, None), (0, None)]
+        ) 
         if con is not None:
             con.execute(
-                '''insert into heston(qS, kappa_v, xi_v, sigma_v, delta, sS1, sS2, sS3, gamma, p, dt, kappa_r, xi_r, f, fun)
-                values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', [*self.x.x, self.n_simulations, self.dt, *self.r_params, self.x.fun])
+                '''insert into heston(qS, kappa_v, xi_v, sigma_v, delta, sS1, sS2, sS3, gamma, sV, p, dt, kappa_r, xi_r, f, fun)
+                values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', [*self.x.x, 0, 0, -999, self.n_simulations, self.dt, *self.r_params, self.x.fun])
             con.commit()
         return self.x
 
 
-def do_calibration(q, t):
+def json_to_sql():
     con = sqlite3.connect('results.sqlite')
-    con.execute('create table if not exists cheyette(kappa real, xi real, created datetime default current_timestamp, p real, dt real, f real, fun real)')
-    con.execute('create table if not exists heston(qS real, kappa_v real, xi_v real, sigma_v real, delta real, sS1 real, sS2 real, sS3 real, gamma real, created datetime default current_timestamp, p real, dt real, kappa_r real, xi_r real, f real, fun real)')
-    forward_curve = .002
     with open('spx_option_data.json') as f:
         js = json.load(f)
     df = pd.Panel(js).loc[:,:,'2017/06/30']
@@ -171,17 +192,24 @@ def do_calibration(q, t):
     df.index = df.index.days/365
     df = df[df.index>0]
     df /= 100
-    ca = Calibration(0.0212, 0.00445, forward_curve, df)
+    df.to_sql('option_data', con, if_exists='replace')
+
+
+def do_calibration(q, t):
+    con = sqlite3.connect('results.sqlite')
+    con.execute('create table if not exists heston(qS real, kappa_v real, xi_v real, sigma_v real, delta real, sS1 real, sS2 real, sS3 real, gamma real, sV real, created datetime default current_timestamp, p real, dt real, kappa_r real, xi_r real, f real, fun real)')
+    forward_curve = .002
+    ca = Calibration(0.0212, 0.00445, forward_curve, con)
     while time.time() < t:
         ca.calibrate(n_simulations=10000, con=con, q=q)
-
 
 
 if __name__ == '__main__':
     con = sqlite3.connect('results.sqlite')
     con.execute('create table if not exists cheyette(kappa real, xi real, created datetime default current_timestamp, p real, dt real, f real, fun real)')
-    con.execute('create table if not exists heston(qS real, kappa_v real, xi_v real, sigma_v real, delta real, sS1 real, sS2 real, sS3 real, gamma real, created datetime default current_timestamp, p real, dt real, kappa_r real, xi_r real, f real, fun real)')
     forward_curve = .002
+
+    json_to_sql()
 
     if False:
         print('started')
@@ -207,6 +235,3 @@ if __name__ == '__main__':
         print('---')
         print('violation', ' '.join(f'{c[0]:12.8f}' for c in out))
         print('gamma    ', ' '.join(f'{c[1]:12.8f}' for c in out))
-
-
-
